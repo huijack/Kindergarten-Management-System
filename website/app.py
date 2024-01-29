ae98e647faac3312df63d96db4d2c4a87d5c2d8f
huijack <jacklim2626@gmail.com>
2024-01-29 15:29:43 +0800

    add hideshow tab

--- website/app.py
+++ website/app.py
@@ -3,10 +3,12 @@ from flask import Flask, render_template
 app = Flask(__name__, template_folder='templates', static_url_path='/static')
 
 @app.route('/')
-
+@app.route('/home')
 
 def index():
     return render_template('index.html')
 
-if __name__ == '__main__':
-    app.run(debug=True)
+
+@app.route('/teacher')
+def teacher():
+    return render_template('teacher.html')

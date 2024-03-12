4aae003bd9b8455aae9852916675557ffd462557
huijack <jacklim2626@gmail.com>
2024-03-12 09:48:04 +0800

    Date fix reload when page reloads

--- website/static/js/date.js
+++ website/static/js/date.js
@@ -1,9 +1,28 @@
-setInterval(function() {
-    let date = new Date();
-    let dateNow = date.toLocaleDateString("en-GB", { day: '2-digit', month: '2-digit', year: 'numeric' }) + " " + date.toLocaleTimeString();
-    let elements = document.querySelectorAll(".date-now");
+function updateDate() {
+  let date = new Date();
+  let dateNow =
+    date.toLocaleDateString("en-GB", {
+      day: "2-digit",
+      month: "2-digit",
+      year: "numeric",
+    }) +
+    " " +
+    date.toLocaleTimeString();
+  let elements = document.querySelectorAll(".date-now");
 
-        elements.forEach(function(element) {
-        element.innerHTML = dateNow;
-    });
-}, 1000);
+  elements.forEach(function (element) {
+    element.innerHTML = dateNow;
+  });
+
+  localStorage.setItem("lastDate", dateNow);
+}
+
+let storedDate = localStorage.getItem("lastDate");
+if (storedDate) {
+  let elements = document.querySelectorAll(".date-now");
+  elements.forEach(function (element) {
+    element.innerHTML = storedDate;
+  });
+}
+
+setInterval(updateDate, 1000);
